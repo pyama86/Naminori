8c2ed131e832ee8e1560fa0bd78634e8bbc2e49c
kazuhiko yamahsita <pyama@pepabo.com>
2015-06-22 23:11:14 +0900

    add http

--- lib/naminori/naminori.rb
+++ lib/naminori/naminori.rb
@@ -7,5 +7,8 @@ require File.dirname(__FILE__) + '/service'
 require File.dirname(__FILE__) + '/service/base'
 require File.dirname(__FILE__) + '/service/configure'
 require File.dirname(__FILE__) + '/service/dns'
+require File.dirname(__FILE__) + '/service/http'
 require 'resolv'
+require 'net/http'
+require 'net/https'
 
---
+++ lib/naminori/service/http.rb
@@ -0,0 +1,32 @@
+#! /usr/bin/env ruby
+module Naminori
+  class Service
+    class Http < Naminori::Service::Base
+
+      def healty?(ip)
+        http = Net::HTTP.new(ip, config.port)
+        http.open_timeout = config.timeout
+
+        begin
+          http.get("/#{@query}")
+        rescue => e
+          false
+        end
+      end
+
+      def default_config
+        {
+          role: "http",
+          port: "80",
+          protocol: ["tcp"],
+          vip: "192.168.77.9",
+          method: "nat",
+          query: "index.html",
+          retry: 3,
+          timeout: 3,
+          notifier: nil
+        }
+      end
+    end
+  end
+end
