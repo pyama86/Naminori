834624198e4f4054e195c81cb89228d10f4cb751
kazuhiko yamashita <pyama@pepabo.com>
2015-06-24 10:34:01 +0900

    slack notifier append time

--- lib/naminori/notifier/slack.rb
+++ lib/naminori/notifier/slack.rb
@@ -5,7 +5,7 @@ module Naminori
       def notifier(type, message)
         icon = type == "add" ? ":white_check_mark:" : ":no_entry_sign:"
         notifier = ::Slack::Notifier.new(config.webhook_url, { channel: config.channel, username: config.user})
-        notifier.ping  icon + message, icon_emoji: ":sparkle:"
+        notifier.ping  icon + Time.new.strftime("%H:%M:%S ") + message, icon_emoji: ":sparkle:"
       rescue => e
         p e
       end
